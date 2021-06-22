8c171545f810b6fdb695b86c84d8864d08867349
Leo <leo.bravar95@gmail.com>
2021-06-22 18:25:46 +0200

    add creator_id to events, make a create form,new controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,26 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = current_user.created_events.build
+    
+  end
+
+  def create
+    @event = current_user.created_events.build(event_params)
+
+    if @event.save
+      flash[:success] = 'Event created successfully'
+
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title,:date,:location,:description)
+  end
 end
