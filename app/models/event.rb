3b33afad1b071013a6bf8305b20afdac8b01cfee
Leo <leo.bravar95@gmail.com>
2021-06-22 18:48:17 +0200

    show nad index page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
+
+    validates :title, presence: true
+    validates :location, presence: true
+    validates :date, presence: true
+    validates :description, presence: true, length: {minimum: 10}
 end
